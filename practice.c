85e039af39d3a87a1c9c338950a25b8b6df2d8f7
Wilsha RW <wilsharw@gmail.com>
2024-06-20 20:42:13 +0200

    nested for loop

--- practice.c
+++ practice.c
@@ -1,13 +1,15 @@
-//asking the user for output//
+//asking the user for output five times//
 
 #include <stdio.h>
 
 int main(void)
 {
-    int n,number, triangular_num;
+    int n,number, triangular_num,counter;
 
-    printf("Enter the triangle number value\n");
-    scanf("%i", &number);
+    for (counter = 1; counter <= 5;counter=counter+1)
+    {
+       printf("Enter the triangle number value\n");
+       scanf("%i", &number);
 
     triangular_num = 0;
 
@@ -15,6 +17,6 @@ int main(void)
         
             triangular_num = triangular_num + n;
             printf("The value for triangle number %i is %i\n", number , triangular_num);
-    
+    }
 return 0;
 }
