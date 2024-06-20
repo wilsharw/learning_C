4b13853f8d7f2ff04b330a82f0bfb1f953eb77ac
Wilsha RW <wilsharw@gmail.com>
2024-06-20 20:13:48 +0200

    for loop example

--- practice.c
+++ practice.c
@@ -1,11 +1,20 @@
-//program to calculate the eighth triangular number//
+//program to calculate the 200th triangular number using for loop//
 
 #include <stdio.h>
+
 int main(void)
 {
-    int triangular_num;
-    triangular_num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
-    printf("The value of the eighth triangular number is %i\n", triangular_num);
+    int n,triangular_num;
+
+    triangular_num = 0;
+
+    for (n = 1; n <= 200; n=n+1)
+        
+            triangular_num = triangular_num + n;
+            printf("The vaue of the 200th triangular_num is %i\n", triangular_num);
+    
+
+
 
     return 0;
 }
