6f02bbad319528da117ccfc8ac419f0981fa4a10
Wilsha RW <wilsharw@gmail.com>
2024-06-23 21:12:51 +0200

    practicing

--- practice.c
+++ practice.c
@@ -4,18 +4,15 @@
 
 int main(void)
 {
-    int n, two_to_the_n;
+    int dollars, cents, count;
 
-    printf("Table of powers of two\n\n");
-    printf(" n        2 to the n\n");
-    printf("____     ___________\n");
-
-    two_to_the_n = 1;
-    for (n = 0; n <= 10;++n)
-      {
-          printf("%2i         %i\n", n, two_to_the_n);
-          two_to_the_n *=2;
-
-      }
-      return 0;
+    for (count = 1; count <= 10;++count)
+    {
+        printf("Enter dollars:");
+        scanf("%i", &dollars);
+        printf("Enter cents: ");
+        scanf("%i", &cents);
+        printf("$%i.%.2i\n\n", dollars, cents);
+    }
+    return 0;
 }
