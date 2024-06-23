99db903569beadf43fd8ba72c1dd6e95ee9b5c9c
Wilsha RW <wilsharw@gmail.com>
2024-06-23 21:37:49 +0200

    practice 4.11

--- practice.c
+++ practice.c
@@ -4,15 +4,22 @@
 
 int main(void)
 {
-    int dollars, cents, count;
+    int sum, n, count;
 
     for (count = 1; count <= 10;++count)
     {
-        printf("Enter dollars:");
-        scanf("%i", &dollars);
-        printf("Enter cents: ");
-        scanf("%i", &cents);
-        printf("$%i.%.2i\n\n", dollars, cents);
+        printf("Enter a non negative integer: ");
+        scanf("%i", &n);
+        sum = 0;
+
+        for (; n != 0; n = n / 10)
+        {
+            sum = sum+ (n % 10) ;
+
+        }
+            printf("the sum of digits is %i\n", sum);
+
     }
-    return 0;
+
+        return 0;
 }
