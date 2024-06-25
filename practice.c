12661aafc7504a94507c99f497f1c044b2ee98e8
Wilsha RW <wilsharw@gmail.com>
2024-06-25 19:42:26 +0200

    if practice 2

--- practice.c
+++ practice.c
@@ -1,16 +1,32 @@
-//program to calculate the absolute value of the integer//
+//program to calculate the grade//
 #include<stdio.h>
 int main (void)
 
 {
-    int number;
-    printf("Type the number:  ");
-    scanf("%i", &number);
+    int number_of_grades, i, grade, failing_count,grade_total;
+    float average;
 
-    if(number<0)
-        number = -number;
+    failing_count = 0;
+    average = 0;
+    grade_total = 0;
 
-    printf("The number is %i\n", number);
+    printf("How many grades will you be entering:   \n");
+    scanf("%i", &number_of_grades);
 
-    return 0;
+    for (i = 1; i <= number_of_grades; ++i)
+
+        {
+        printf("Enter the grade #%i:  \n", i);
+        scanf("%i", &grade);
+        grade_total = grade_total + grade;
+        
+        if(grade<65)
+            ++failing_count;
+
+        average = (float)grade_total / number_of_grades;
+        }
+        printf("The average of the grades is :%f\n", average);
+        printf("The number of failing test grades are:%i\n", failing_count);
+
+        return 0;
 }
