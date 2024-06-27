da0dece5e19713ceb3801327e07e5193698629e8
Wilsha RW <wilsharw@gmail.com>
2024-06-27 20:50:52 +0200

    exercise 5.9_switch statement

--- practice.c
+++ practice.c
@@ -1,20 +1,39 @@
-//Program exercise 5.6_nested if else//
+//Program exercise 5.9_switch statement//
+//program to evalute simple expressions of the form value operator//
 
 #include<stdio.h>
 int main (void)
 
 {
-    char c;
+    float value_1,value_2;
+    char operator;
 
-    printf("Please enter a single character:");
-    scanf("%c", &c);
+    printf("Enter the expression:\n");
+    scanf("%f%c%f", &value_1, &operator, &value_2);
 
-    if ((c>='A' && c<='Z') || (c>='a' && c<='z'))
-        printf("It is a alphabetical character");
-    else if (c>='0' && c<='9')
-        printf("It is a numerical character");
-    else
-        printf("It is a special character");
-
-    return 0;
+    switch(operator)
+        {
+            case'+':
+              printf("%f\n",value_1 + value_2);
+              break;
+            case'-':
+              printf("%f\n",value_1 - value_2);
+              break;
+            case'*':
+              printf("%f\n",value_1 * value_2);
+              break;
+            case'/':
+              if(value_2==0)
+              printf("Division by zero\n");
+              else
+              printf("%f\n", value_1/value_2);
+              break;
+            default:
+                printf("Unknown operator\n");
+                break;
+            }
+            return 0;
 }
+
+   
+
